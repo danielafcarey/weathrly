b12d3d3d161b194622d95bb824a8756c630816e1
Daniela Carey <danielafcarey@gmail.com>
2018-05-22 14:11:23 -0600

    Delete Api.js

--- src/Api.js
+++
@@ -1,7 +0,0 @@
-const fetchApi = (location) => {
-  return (
-    fetch(`http://api.wunderground.com/api/81347f06b321e144/conditions/forecast10day/hourly10day/q/${ location }.json`)
-  )
-}
-
-export default fetchApi;
